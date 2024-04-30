1fb149f850aef01c2b0060f12213916bdd6155d7
javeriam911 <javeriam911@gmail.com>
2024-05-01 01:15:31 +0500

    Data types

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict";
+console.log("class third");
+///strong and explicit data types
+/////string data type
+let firstName = "jiya";
+console.log(firstName);
+//////// number
+let score = 22;
+console.log(score);
+//////   boolean
+let bool = true;
+console.log(bool);
+let booool = false;
+console.log(booool);
+///infer data type
+let girl = "jiya";
+let num1 = 23;
+console.log(girl, num1);
---
+++ main.ts
@@ -0,0 +1,26 @@
+console.log("class third")
+
+///strong and explicit data types
+/////string data type
+
+
+let firstName  : string = "jiya";
+console.log(firstName)
+//////// number
+let score : number = 22;
+console.log(score)
+
+//////   boolean
+
+let bool : boolean = true;
+console.log(bool);
+let booool : boolean = false;
+console.log(booool)
+
+
+///infer data type
+
+let girl = "jiya";
+let num1 = 23
+
+console.log(girl , num1)
